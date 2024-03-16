ec4ddb96bb1b561dc55d54f07a3735f6f14bfb0d
Giuseppe Pennisi <giuseppe.pennisi11@gmail.com>
2024-03-16 19:55:49 +0100

    Initial commit

---
+++ apps/pokedex-e2e/cypress.config.ts
@@ -0,0 +1,17 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      webServerCommands: {
+        default: 'nx run pokedex:serve:development',
+        production: 'nx run pokedex:serve:production',
+      },
+      ciWebServerCommand: 'nx run pokedex:serve-static',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
