d4e708b607763e2b8fdf7b88a17a8c94006f8cc1
Giuseppe Pennisi <giuseppe.pennisi11@gmail.com>
2024-07-26 16:13:09 +0200

    :sparkles: feat(ui): added pokeball loader

--- libs/pokeball-loader-ui/src/lib/pokeball-loader-ui/pokeball-loader-ui.component.ts
+++ libs/pokeball-loader-ui/src/lib/pokeball-loader-ui/pokeball-loader-ui.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
     selector: 'pkmn-pokeball-loader-ui',
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
     styleUrls: ['./pokeball-loader-ui.components.scss'],
     templateUrl: './pokeball-loader-ui.component.html',
 })
-export class PokeballLoaderUiComponent {}
+export class PokeballLoaderUiComponent {
+    /*  Introdotto con la versione 17 di Angular.
+        Poiché il signal non è writable, non possiamo settare il valore del signal, ma solo accettare il valore del signal dal'elemento padre.
+        https://angular.io/api/core/InputSignal
+    */
+    isLoading = input.required<boolean>();
+}
